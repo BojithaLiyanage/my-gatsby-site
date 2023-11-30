98822cdb6402cb861d7a191ba791056fbb79580f
BojithaLiyanage <118416893+BojithaLiyanage@users.noreply.github.com>
2023-11-30 19:04:12 +0530

    ,

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,27 @@
 import * as React from "react";
-import { Link } from "gatsby";
 import {
   container,
   heading,
   navLinks,
   navLinkItem,
   navLinkText,
+  siteTitle,
 } from "./layout.module.css";
-import { StaticImage } from "gatsby-plugin-image";
+import { Link, useStaticQuery, graphql } from "gatsby";
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
   return (
     <div className={container}>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}>
