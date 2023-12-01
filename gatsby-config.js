96a2e92dee5e979956a71fa15a069cf55ece8b4e
BojithaLiyanage <118416893+BojithaLiyanage@users.noreply.github.com>
2023-12-01 12:56:56 +0530

    m

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,17 @@ module.exports = {
     title: `My Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog/`,
+      },
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+  ],
 };
